0ee4f6a8f0b628fa23873f70a50df4bfc39893f6
Erika Jissel Gutierrez Beltran <erika.giselle.gb@hotmail.com>
2016-07-02 11:13:08 -0500

    Se añadieron los archivos

---
+++ www/js/app.js
@@ -0,0 +1,94 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+var myApp = angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'ui.router'])
+
+myApp.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+myApp.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom');
+  
+  /*$stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/inicioSesion.html"
+    })*/
+    /*.state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/facts2.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/about.html"
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "templates/nav-stack.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "templates/contact.html"
+        }
+      }
+    });*/
+
+
+   //$urlRouterProvider.otherwise("/tab/inicio");
+
+})
+
+
+
+
+
+
+
---
+++ www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('app.controllers', [])
+  
+.controller('inicioSesionCtrl', function($scope) {
+
+})
+   
+.controller('registroCtrl', function($scope) {
+
+})
+   
+.controller('inicioCtrl', function($scope) {
+
+})
+   
+.controller('perfilCtrl', function($scope) {
+
+})
+   
+.controller('restaurantesCtrl', function($scope) {
+
+})
+   
+.controller('menuRestCtrl', function($scope) {
+
+})
+   
+.controller('menuBarCtrl', function($scope) {
+
+})
+   
+.controller('barCtrl', function($scope) {
+
+})
+   
+.controller('eventosCtrl', function($scope) {
+
+})
+   
+.controller('reservaResCtrl', function($scope) {
+
+})
+   
+.controller('reservaRes_conAlimentosCtrl', function($scope) {
+
+})
+   
+.controller('reservaBarCtrl', function($scope) {
+
+})
+ 
